ae8bf1c3019be259ffd2eb844196225d6e085304
Talaat Ramadan <talatramadan10@gmail.com>
2023-09-24 21:59:42 +0300

    Create Category Model

---
+++ EcommerceWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
