3e18562a90a8881deb9e5e3b8bb0c481d76b71ec
Talaat Ramadan <talatramadan10@gmail.com>
2023-10-06 10:07:41 +0300

    feat: Add Image Url Column to Product

--- Ecommerce.Models/Product.cs
+++ Ecommerce.Models/Product.cs
@@ -32,5 +32,6 @@ namespace Ecommerce.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category? Category { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
