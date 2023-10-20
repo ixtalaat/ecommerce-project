5cce912561727a7b456f556594762e9bcca23f70
Talaat Ramadan <talatramadan10@gmail.com>
2023-10-20 03:39:50 +0300

    Fix Scaffold Identity Issue (NET8)

--- Ecommerce.DataAccess/Data/ApplicationDbContext.cs
+++ Ecommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
