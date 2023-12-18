e97c403a46e841e4b611592b6743fed8c7e46de2
Talaat Ramadan <talatramadan10@gmail.com>
2023-12-18 08:06:05 +0200

    Configure Stripe in Project

---
+++ Ecommerce.Utility/StripeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Utility
+{
+    public class StripeSettings
+    {
+        public string SecretKey { get; set; } = null!;
+        public string PublishableKey { get; set; } = null!;
+    }
+}
