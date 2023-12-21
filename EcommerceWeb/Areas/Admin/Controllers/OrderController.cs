d51bea4a55cc5e10d98ce70aef2b71ff9f67f200
Talaat Ramadan <talatramadan10@gmail.com>
2023-12-21 04:58:23 +0200

    Add Filters in Order List

--- EcommerceWeb/Areas/Admin/Controllers/OrderController.cs
+++ EcommerceWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using Ecommerce.DataAccess.IRepository;
 using Ecommerce.Models;
+using Ecommerce.Utility;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace EcommerceWeb.Areas.Admin.Controllers
 {
@@ -20,9 +22,28 @@ namespace EcommerceWeb.Areas.Admin.Controllers
         }
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+            switch (status)
+            {
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProcess);
+                    break;
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
             return Json(new { data = orderHeaders });
         }
         #endregion
