744a504a200a14237c21ba9b7a455440c3598370
Talaat Ramadan <talatramadan10@gmail.com>
2023-12-28 14:14:34 +0200

    Create Shopping Cart View Component

---
+++ EcommerceWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using Ecommerce.DataAccess.IRepository;
+using Ecommerce.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EcommerceWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity!;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim is not null)
+            {
+                if (HttpContext.Session.GetInt32(StaticDetails.SessionCart) is null)
+                {
+                    HttpContext.Session.SetInt32(StaticDetails.SessionCart,
+                        _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value).Count());
+                }
+                return View(HttpContext.Session.GetInt32(StaticDetails.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+
+        }
+    }
+}
