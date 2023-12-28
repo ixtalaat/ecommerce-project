daa95ac760932188dfbd09d485f11f5170d82fb8
Talaat Ramadan <talatramadan10@gmail.com>
2023-12-28 12:42:00 +0200

    Implement Session for Shopping Cart

--- EcommerceWeb/Areas/Customer/Controllers/HomeController.cs
+++ EcommerceWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.DataAccess.IRepository;
 using Ecommerce.Models;
+using Ecommerce.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -46,15 +47,17 @@ namespace EcommerceWeb.Areas.Customer.Controllers
             if (cartFromDb is not null) {
                 cartFromDb.Count += shoppingCart.Count;
                 _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
             }
             else
             {
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(StaticDetails.SessionCart, 
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
             }
-
             TempData["success"] = "Cart updated successfully";
 
-            _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
         }
 
